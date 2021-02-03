ae05e702e5b332b412ea3570f889aa10821879af
Iliescu Mihail Doirn <iliescu.dorin@gmail.com>
2021-02-04 01:08:59 +0200

    GUI reuploaded
    reuploaded from Github Desktop and splitted in order to be <100mb (the Github limit)

---
+++ GUI Application/utils.py
@@ -0,0 +1,62 @@
+from pathlib import Path
+
+import tensorflow as tf
+import numpy as np
+
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+
+import tensorflow_hub as hub
+
+categories = ['afraid', 'angry', 'disgusted', 'happy', 'neutral', 'sad', 'surprised']
+image_size = 224
+
+working_path = str(Path().absolute())
+
+inception_path = working_path + '\Saved Models\inception_v3.h5'
+resnet_path = working_path + '\Saved Models\\Resnet-v2.h5'
+mobilenet_path = working_path + '\Saved Models\mobilenet.h5'
+print("Loading Models.....")
+inception_model = load_model(inception_path, custom_objects={'KerasLayer': hub.KerasLayer})
+resnet_model = load_model(resnet_path, custom_objects={'KerasLayer': hub.KerasLayer})
+mobilenet_model = load_model(mobilenet_path)
+models = [inception_model, resnet_model, mobilenet_model]
+models_name = ["Inception", "Resnet", "MobileNet"]
+print("Models Loaded")
+
+
+def ensemble(x, weights, models):
+    '''
+    returns a weighted average of predictions made by the models\n
+    x -> input image \n
+    weights -> a list of weights \n
+    models -> a list of models\n    
+    '''
+    outputs = []
+    for model in models:
+        outputs.append(list(model.predict(x)[0]))
+
+    outputs = np.array(outputs)
+    avg = np.average(a=outputs, axis=0, weights=weights)
+    return avg
+
+
+def predict(input_image):
+    '''
+    returns a predicted class, associated probability
+    input_image = an image
+    models = list of models    
+    '''
+    weights = [  0.228, 0.601,0.172]
+    input_image = input_image / 255.0
+    img = input_image.reshape(-1, image_size, image_size, 3)
+    model_predictions = []
+    model_probs = []
+    for i in range(len(models)):
+        print(models_name[i], categories[np.argmax(models[i].predict(img))])
+        model_predictions.append(categories[np.argmax(models[i].predict(img))])
+        model_probs.append(models[i].predict(img))
+    avg_pred = ensemble(img, weights, models)
+    if model_predictions.count(categories[0]) == 1:
+        print(categories[0], model_probs[model_predictions.index(categories[0])][0])
+    return categories[np.argmax(avg_pred)], avg_pred
